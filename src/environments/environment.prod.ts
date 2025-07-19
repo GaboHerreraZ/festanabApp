e8bef70a78ee4c97ee4cd81a0e42a3c25167487b
Gabriel Herrera Z <gherreraz@lean-tech.io>
2025-07-19 06:28:00 -0500

    updae environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    production: true,
+    production: false,
     supabaseUrl: 'https://kwdkuvdnnivftaflxpoz.supabase.co',
-    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3ZGt1dmRubml2ZnRhZmx4cG96Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjI4Mzg1MCwiZXhwIjoyMDY3ODU5ODUwfQ.Y9fKOlgj6FQVy-KMVFQ0X-Ohq-igghWpxg1OrVj63Yc'
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt3ZGt1dmRubml2ZnRhZmx4cG96Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjI4Mzg1MCwiZXhwIjoyMDY3ODU5ODUwfQ.Y9fKOlgj6FQVy-KMVFQ0X-Ohq-igghWpxg1OrVj63Yc',
+    festanabApi: 'http://localhost:3000/api'
 };
